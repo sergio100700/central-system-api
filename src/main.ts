8413b1b139343c03f7ad8fccaf1979a763cacf97
sergio lopez loureiro <lopez.loureiro.sergio.ald@gmail.com>
2026-03-27 20:58:05 +0100

    chore: README.md

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Central System API')
     .setDescription('API REST for Organizations and ChargePoints')
@@ -16,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(Number(process.env.PORT ?? 3001), '0.0.0.0');
 }
 bootstrap();
